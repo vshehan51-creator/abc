a97a73a713795cac3dec57ad75cb48075f47ca12
vshehan51-creator <vshehan51@gmail.com>
2025-11-27 07:13:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = {
-
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+module.export𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=9c94d0caae138672: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
